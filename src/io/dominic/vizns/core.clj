fb2391a4af24246ac4e46f3a573970ba5b7c92de
Dominic Monroe <dmc@juxt.pro>
2020-02-15 14:06:00 +0000

    Add navigation output type

--- src/io/dominic/vizns/core.clj
+++ src/io/dominic/vizns/core.clj
@@ -98,72 +98,92 @@
             :when lib]
         [(:from ns-relation) lib]))))
 
-(defn- dot-graph
+(defn- make-lib-info
   [relations lib-map deps-map]
   (let [used-deps (set (map second relations))]
-    (dot/digraph
-      (concat
-        [{:overlap "false"
-          :layout "neato"}]
-        (map
-          (fn [lib]
-            [(keyword lib)
-             (merge
-               {:shape :cylinder
-                :label (str lib)
-                ;; Needed for cylinder shape
-                :margin "0.35,0.1"
-                :style :filled
-                :fillcolor (if (contains? used-deps lib)
-                             :green
-                             :grey)})])
-          (keys (:deps deps-map)))
-        (map
-          (fn [lib]
-            [(keyword lib)
-             {:shape :cylinder
-              :label (str lib)
-              :margin "0.35,0.1"
-              :fillcolor :red
-              :style :filled}])
-          (set/intersection
-            (set/difference
-              (set (keys lib-map))
-              (set (keys (:deps deps-map))))
-            used-deps))
-        (map
-          (fn [[ns lib]]
-            [(keyword ns) (keyword lib)])
-          relations)))))
+    (zipmap
+      (keys lib-map)
+      (for [[lib data] lib-map]
+        (merge data
+               {::used? (contains? used-deps lib)
+                ::transitive? (not (contains? (:deps deps-map) lib))})))))
 
-(defn -main
-  [& args]
-  (let [{{:keys [deps-file show? format output]} :options}
-        (cli/parse-opts args
-                        [["-d" "--deps DEPS-FILE" "Path to deps file"
-                          :id :deps-file
-                          :default (.getCanonicalFile (io/file "deps.edn"))
-                          :parse-fn #(.getCanonicalFile (io/file %))
-                          :validate [#(.exists %)]]
-                         [nil "--show" "Show diagram then exit"
-                          :id :show?
-                          :parse-fn #(new Boolean %)
-                          :default false]
-                         ["-f" "--format FORMAT" "One of dot,png,svg"
-                          :default "dot"
-                          :validate [#{"dot" "png" "svg"}]]
-                         ["-o" "--output OUTPUT" "Where to output the graph (not compatible with --show).  Use - for STDOUT"
-                          :default "deps-graph.dot"]])
+(defn- escape-fname
+  [sym]
+  (string/escape (str (if (= (namespace sym) (name sym))
+                        (name sym)
+                        sym))
+                 {\/ "__"}))
+
+(defn- make-lib-node
+  [lib resolved-dep+ opts]
+  (let [{:keys [url?]} opts]
+    [(keyword lib)
+     (merge
+       {:shape :cylinder
+        :label (str lib)
+        ;; Needed for cylinder shape
+        :margin "0.35,0.1"
+        :style :filled
+        :fillcolor (cond
+                     (and
+                       (::transitive? resolved-dep+)
+                       (::used? resolved-dep+)) :red
+
+                     (::used? resolved-dep+) :green
+
+                     (not (::transitive? resolved-dep+)) :grey
+                     
+                     (::transitive? resolved-dep+) :blue)}
+       (when url?
+         {:URL (str "./" (escape-fname lib) ".svg")}))]))
+
+(defn- make-ns-node
+  [ns opts]
+  (let [{:keys [url?]} opts]
+    [(keyword ns)
+     (merge
+       {:label (str ns)}
+       (when url?
+         {:URL (str "./" (escape-fname ns) ".svg")}))]))
+
+(defn- dot-graph
+  [relations nodes]
+  (dot/digraph
+    (concat
+      [{:overlap "false"
+        :layout "neato"}]
+      nodes
+      (map
+        (fn [[ns lib]]
+          [(keyword ns) (keyword lib)])
+        relations))))
+
+(defn- single
+  [common-opts single-opts]
+  (let [{:keys [deps-file]} common-opts
+        {:keys [show? format output]} single-opts
         deps-map (read-deps deps-file)
         lib-map (lib-map deps-map deps-file)
-        graph (dot-graph
-                (relations
-                  lib-map
-                  (map #(io/file (.getParent deps-file) %)
-                       (:paths deps-map)))
-                lib-map
-                deps-map)
-        dot (dot/dot graph)
+        relations (relations
+                    lib-map
+                    (map #(io/file (.getParentFile deps-file) %)
+                         (:paths deps-map)))
+        lib-info (make-lib-info relations lib-map deps-map)
+        lib-nodes (zipmap (keys lib-map)
+                          (for [[lib resolved-dep+] lib-info]
+                            (make-lib-node
+                              lib resolved-dep+
+                              {:url? true})))
+        ns-nodes (zipmap (map first relations)
+                         (map #(make-ns-node % {:url? true})
+                              (map first relations)))
+        dot (dot/dot
+              (dot-graph relations
+                         (concat (vals (select-keys
+                                         lib-nodes
+                                         (map second relations)))
+                                 (vals ns-nodes))))
         output (if (= "-" output)
                  System/out
                  (io/file
@@ -172,11 +192,92 @@
                      (str "." format))))]
     (cond
       show?
-      (do (dot.jvm/show! dot)
-          (System/exit 0))
+      (do
+        (dot.jvm/show! dot)
+        (println "Hit Ctrl-C to exit"))
 
       (= format "dot")
       (spit (io/output-stream output) dot)
 
       :else
       (dot.jvm/save! dot output {:format (keyword format)}))))
+
+(defn- navigate
+  [common-opts navigate-opts]
+  (let [{:keys [deps-file]} common-opts
+        {:keys [output]} navigate-opts
+        deps-map (read-deps deps-file)
+        lib-map (lib-map deps-map deps-file)
+        relations (relations
+                    lib-map
+                    (map #(io/file (.getParentFile deps-file) %)
+                         (:paths deps-map)))
+        lib-info (make-lib-info relations lib-map deps-map)
+        lib-nodes (zipmap (keys lib-map)
+                          (for [[lib resolved-dep+] lib-info]
+                            (make-lib-node
+                              lib resolved-dep+
+                              {:url? true})))
+        ns-nodes (zipmap (map first relations)
+                         (map #(make-ns-node % {:url? true})
+                              (map first relations)))]
+    (dot.jvm/save! (dot/dot
+                     (dot-graph relations
+                                (concat (vals (select-keys
+                                                lib-nodes
+                                                (map second relations)))
+                                        (vals ns-nodes))))
+                   (doto (io/file output "root.svg")
+                     io/make-parents)
+                   {:format :svg})
+    (doseq [[lib relations] (group-by second relations)]
+      (dot.jvm/save! (dot/dot
+                       (dot-graph relations
+                                  (cons (get lib-nodes lib)
+                                        (vals (select-keys ns-nodes (map first relations))))))
+                     (io/file output (str (escape-fname lib) ".svg"))
+                     {:format :svg}))
+    (doseq [[ns relations] (group-by first relations)]
+      (dot.jvm/save!
+        (dot/dot
+          (dot-graph relations
+                     (cons (get ns-nodes ns)
+                           (vals (select-keys lib-nodes (map second relations))))))
+        (io/file output (str (escape-fname ns) ".svg"))
+        {:format :svg}))))
+
+(defn -main
+  [& args]
+  (let [{common-opts :options
+         arguments :arguments}
+        (cli/parse-opts
+          args
+          [["-d" "--deps DEPS-FILE" "Path to deps file"
+            :id :deps-file
+            :default (.getCanonicalFile (io/file "deps.edn"))
+            :parse-fn #(.getCanonicalFile (io/file %))
+            :validate [#(.exists %)]]]
+          :in-order true)
+        {command-opts :options}
+        (cli/parse-opts
+          (rest arguments)
+          (case (first arguments)
+            "single"
+            [[nil "--show" "Show diagram then exit"
+              :id :show?
+              :parse-fn #(new Boolean %)
+              :default false]
+             ["-f" "--format FORMAT" "One of dot,png,svg"
+              :default "dot"
+              :validate [#{"dot" "png" "svg"}]]
+             ["-o" "--output OUTPUT" "Where to output the graph (not compatible with --show).  Use - for STDOUT"
+              :default "deps-graph.dot"]]
+
+            "navigate"
+            [["-o" "--output OUTPUT" "Folder to output the graph to.  Will be created if does not exist."
+              :parse-fn io/file
+              :default "deps-graph/"]]))]
+    ((case (first arguments)
+       "single" single
+       "navigate" navigate)
+     common-opts command-opts)))
