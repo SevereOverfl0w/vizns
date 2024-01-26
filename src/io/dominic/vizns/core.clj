52eae0364284ad191e71f6591ef927665b1f37bc
kaspazza <kaspazza@gmail.com>
2024-01-26 13:47:10 +0100

    Fix for non-existing paths breaking in kondo

--- src/io/dominic/vizns/core.clj
+++ src/io/dominic/vizns/core.clj
@@ -159,6 +159,15 @@
           [(keyword ns) (keyword lib)])
         relations))))
 
+(defn- existing-parent-files [parent-dir paths]
+  (reduce (fn [acc path]
+             (let [file (io/file parent-dir path)]
+               (if (.exists file)
+                 (conj acc path)
+                 acc)))
+          []
+          paths))
+
 (defn- single
   [common-opts single-opts]
   (let [{:keys [deps-file]} common-opts
@@ -167,8 +176,7 @@
         lib-map (lib-map deps-map deps-file)
         relations (relations
                     lib-map
-                    (map #(io/file (.getParentFile deps-file) %)
-                         (:paths deps-map)))
+                    (existing-parent-files (.getParentFile deps-file) (:paths deps-map)))
         lib-info (make-lib-info relations lib-map deps-map)
         lib-nodes (zipmap (keys lib-map)
                           (for [[lib resolved-dep+] lib-info]
@@ -210,8 +218,7 @@
         lib-map (lib-map deps-map deps-file)
         relations (relations
                     lib-map
-                    (map #(io/file (.getParentFile deps-file) %)
-                         (:paths deps-map)))
+                    (existing-parent-files (.getParentFile deps-file) (:paths deps-map)))
         lib-info (make-lib-info relations lib-map deps-map)
         lib-nodes (zipmap (keys lib-map)
                           (for [[lib resolved-dep+] lib-info]
